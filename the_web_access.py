780505aa9abe0f38f6837dda6d237f6831dfc332
Milan Solanki <66133186+freshprinceofcoders@users.noreply.github.com>
2021-07-25 23:09:04 +0100

    MAd

--- the_web_access.py
+++ the_web_access.py
@@ -1,6 +1,17 @@
-import requests
-from bs4 import BeautifulSoup
-import smtplib
-from http import server
-import time
-url = 'https://www.skyscanner.net/'
+from selenium import webdriver
+
+driver = webdriver.Chrome("./chromedriver.exe")
+
+driver.get("https://www.skyscanner.net/")
+
+Home = "LHR"
+From = driver.find_element_by_xpath('//*[@id="fsc-origin-search"]')
+From.send_keys(Home)
+
+Destination = "Del"
+To = driver.find_element_by_xpath('//*[@id="fsc-destination-search"]')
+To.send_keys(Destination)
+
+Direct = driver.find_element_by_xpath('//*[@id="flights-search-controls-root"]/div/div/form/div[2]/div[4]/label[1]')
+Direct.click()
+#driver.close()
